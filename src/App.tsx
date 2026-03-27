13121dd6a761c3c0f006f11914f2d044d604f0a0
aoilv99 <aoiiiiilv999@gmail.com>
2026-03-27 16:23:27 +0900

    環境構築した

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4">Hello, World!</h1>
+        <p className="text-lg text-gray-600">Welcome to my portfolio website.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
