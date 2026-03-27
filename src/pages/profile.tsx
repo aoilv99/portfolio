40bf8a7db1774554b03e3af74d0378f854b7d643
aoilv99 <aoiiiiilv999@gmail.com>
2026-03-27 18:18:55 +0900

    1枚目のプロフィール画面の枠組みできた

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom';
+import profileBackgroundImg from '../assets/profile-background.png';
 
 function About() {
   return (
     <div>
-      
+      <nav className='top-nav'>
+          <ul>
+            <li><Link to="/">Top</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
+        </nav>
+      <div className='profile-name'>
+        <h3>小野澤 葵</h3>
+        <p>-onozawa aoi-</p>
+      </div>
+      <div className='profile-comments'>
+        <p className='profile-comments-title'>comments</p>
+        <p>可愛いものが大好きな専門学生です。</p>
+        <p>家族や友達、誰かの心がちょっと温まるような作品 <br /> を作るのが好きです。</p>
+        <p>作品を作るときはユーザーの視点に立ってUI,UXの <br /> 設計をすることを心がけています。</p>
+        <p>人との縁を大切にし、周りをうまく巻き込んで様々 <br /> なことに挑戦するのが得意です。</p>
+      </div>
+      <div className='profile-background'>
+        <img src={profileBackgroundImg} alt="Profile Background" />
+      </div>
     </div>
   );
 }
